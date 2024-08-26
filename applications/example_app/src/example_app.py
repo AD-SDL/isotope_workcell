c1d60716942ddb4b5c90b3de6450a3fa6f6ab4f8
AileenCleary <acleary@anl.gov>
2024-08-26 12:10:17 -0500

    Syntax changes to example_app

--- applications/example_app/src/example_app.py
+++ applications/example_app/src/example_app.py
@@ -2,6 +2,7 @@
 """Example experiment application that uses the WEI client to run a test workflow."""
 
 import json
+import time
 from pathlib import Path
 
 from wei import ExperimentClient
@@ -12,10 +13,12 @@ def main() -> None:
     exp = ExperimentClient("localhost", "8000", "mir_test")
     wf_path = Path(__file__).parent.parent / "workflows" / "test_workflow.yaml"
 
-    for _ in range(5): # Maybe "test_workflow" should just input # of times to run, or other experiment params, and links to mir_module workflow which contains the three actions, instead of looping submit workflow.
+    for _ in range(20): # Maybe "test_workflow" should just input # of times to run, or other experiment params, and links to mir_module workflow which contains the three actions, instead of looping submit workflow.
         run_info = exp.start_run(
             wf_path.resolve(),
         )
+        ## RUN CAMERAS
+        time.sleep(150)
 
     print(json.dumps(run_info, indent=2))
 
